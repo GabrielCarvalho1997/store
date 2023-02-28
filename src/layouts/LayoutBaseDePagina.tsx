7801439bb9a0f811dac3fad2a370fb9c011ae323
Gabriel Carvalho <gabrielcoelhoneves@hotmail.com>
2023-02-28 19:37:44 -0300

    AGORA TA CERTO

--- src/layouts/LayoutBaseDePagina.tsx
+++ src/layouts/LayoutBaseDePagina.tsx
@@ -1,23 +1,24 @@
 import { Box } from '@mui/system';
 import { BtnMenuLateral } from 'components/btnMenuLateral/BtnMenuLateral';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { Typography, useTheme } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import { BarraDePesquisa } from 'components';
+import { useMatch } from 'react-router-dom';
 
 type Props = {
-  children: ReactNode;
+  children?: ReactNode;
   titulo: string;
-  barraDePesquisa: ReactNode;
 };
 
-export const LayoutBaseDePagina = ({
-  children,
-  titulo,
-  barraDePesquisa,
-}: Props) => {
+export const LayoutBaseDePagina = ({ children, titulo }: Props) => {
+  const [texto, setTexto] = useState('');
+
   const theme = useTheme();
   const sm = useMediaQuery(theme.breakpoints.down('sm'));
   const md = useMediaQuery(theme.breakpoints.down('md'));
+
+  const match = useMatch({ path: 'produtos/:id', end: false });
 
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
@@ -33,8 +34,6 @@ export const LayoutBaseDePagina = ({
         gap={1}
         height={theme.spacing(sm ? 6 : md ? 8 : 12)}
       >
-        <BtnMenuLateral />
-
         <Typography
           variant={sm ? 'h5' : md ? 'h4' : 'h3'}
           component="h1"
@@ -45,8 +44,16 @@ export const LayoutBaseDePagina = ({
           {titulo}
         </Typography>
       </Box>
-
-      {barraDePesquisa && <Box>{barraDePesquisa}</Box>}
+      {!match && (
+        <Box marginBottom={4}>
+          <BarraDePesquisa
+            mostrarInputBusca
+            textoBotaoNovo="Pesquisar"
+            texto={texto}
+            setTexto={setTexto}
+          />
+        </Box>
+      )}
 
       <Box flex={1} overflow="auto">
         {children}
